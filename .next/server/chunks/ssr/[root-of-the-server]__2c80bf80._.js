2ce5aca0a3a39de67192cfc67d137b99c40737d0
iamarpitahuja <iamarpitahuja@gmail.com>
2025-08-04 23:02:52 -0400

    Started setting up eco Echo

--- .next/server/chunks/ssr/[root-of-the-server]__2c80bf80._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__2c80bf80._.js
@@ -891,7 +891,7 @@ const SideBar = ()=>{
                                         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$components$2f$NavItem$2e$tsx__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["NavItem"], {
                                             icon: __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$lucide$2d$react$2f$dist$2f$esm$2f$icons$2f$mic$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__$3c$export__default__as__Mic$3e$__["Mic"],
                                             label: "Eco Echo",
-                                            href: "/application-hub",
+                                            href: "/eco-echo",
                                             isCollapsed: isCollapsed
                                         }, void 0, false, {
                                             fileName: "[project]/src/app/components/Sidebar.tsx",
@@ -1060,7 +1060,7 @@ const sidebarRoutes = [
     "/profile",
     "/ec-db",
     "/scholarship-db",
-    "/application-hub",
+    "/eco-echo",
     "/community",
     "/interview-ai",
     "/research-hub",
